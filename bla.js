465c875bdf111f02863db17998358840802640da
octodoger <octodoger@octodoger.oct>
2015-09-03 13:52:51 +0300

    new file

---
+++ bla.js
@@ -0,0 +1,23 @@
+// Hello.
+//
+// ------
+//
+
+function main() {
+  return "Hello, World!";
+}
+
+function main2() {
+  return "Hello, World!"
+}
+
+function main2() {
+  return "Hello, World!"
+}
+
+function main3 () {
+  if (bla. == null) return;
+  return "Hello, World!"
+}
+
+main();
